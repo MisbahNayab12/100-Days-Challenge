c958ef0f3eb438b5c026cc43d197d9586ca7874b
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-03-10 16:04:32 +0500

    Add files via upload

---
+++ Day1.js
@@ -0,0 +1,10 @@
+// Question 2
+var personName = "Ali";
+console.log("Hello ".concat(personName, ", would you like to learn some typescript today?"));
+// Question 3
+// lower case
+console.log(personName.toLowerCase());
+// Upper Case
+console.log(personName.toUpperCase());
+// Title Case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ Day1.ts
@@ -0,0 +1,14 @@
+// Question 2
+let personName: string = "Ali"
+console.log(`Hello ${personName}, would you like to learn some typescript today?`)
+
+
+// Question 3
+// lower case
+console.log(personName.toLowerCase())
+
+// Upper Case
+console.log(personName.toUpperCase())
+
+// Title Case
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase())
