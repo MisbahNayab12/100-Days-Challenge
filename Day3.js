d5c68ce5fc494fe2a21a586e007841f3e85c8f7a
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-03-10 20:24:19 +0500

    Add files via upload

---
+++ Day3.js
@@ -0,0 +1,22 @@
+// Question 7
+var num1 = 2;
+var num2 = 6;
+console.log(num1 + num2);
+var num3 = 10;
+var num4 = 2;
+console.log(num3 - num4);
+var student = 4;
+var classroom = 2;
+console.log(student * classroom);
+var copies = 16;
+var bags = 2;
+console.log(copies / bags);
+// Question 8
+console.log(2 + 6);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//Question 9
+var favouriteNumber = 6;
+var message = "My favourite number is ".concat(favouriteNumber);
+console.log(message);
---
+++ Day3.ts
@@ -0,0 +1,27 @@
+// Question 7
+let num1 = 2
+let num2 = 6
+console.log(num1 + num2)
+
+let num3 = 10
+let num4 = 2
+console.log(num3 - num4)
+
+let student: number = 4
+let classroom: number = 2
+console.log(student * classroom)
+
+let copies: number = 16
+let bags: number = 2
+console.log(copies / bags)
+
+// Question 8
+console.log(2 + 6)
+console.log(10 - 2)
+console.log(4 * 2)
+console.log(16 / 2)
+
+//Question 9
+let favouriteNumber: number = 6
+let message = `My favourite number is ${favouriteNumber}`
+console.log(message)
