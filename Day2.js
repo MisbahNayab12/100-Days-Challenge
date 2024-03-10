85df5e9cd05f31052cc424c29a7317a368bc5195
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-03-10 16:37:56 +0500

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,10 @@
+// Question 4
+console.log('Quaid-e-Azam once said, "There are two powers in the world; one is the sword and other is the pen."');
+// Question 5
+var famous_Person = "Quaid-e-Azam";
+var message = "There are two powers in the world; one is the sword and other is the pen";
+console.log("".concat(famous_Person, " once said, \"There are two powers in the world; one is the sword and other is the pen.\""));
+// Question 6
+var personName = "\t\n Misbah \t\n";
+console.log(personName);
+console.log(personName.trim());
---
+++ Day2.ts
@@ -0,0 +1,12 @@
+// Question 4
+console.log('Quaid-e-Azam once said, "There are two powers in the world; one is the sword and other is the pen."')
+
+// Question 5
+let famous_Person: string = "Quaid-e-Azam"
+let message: string = "There are two powers in the world; one is the sword and other is the pen"
+console.log(`${famous_Person} once said, "There are two powers in the world; one is the sword and other is the pen."`)
+
+// Question 6
+let personName: string = "\t\n Misbah \t\n"
+console.log(personName)
+console.log(personName.trim())
